7f7c20ff28d706df81423e1ce4f550efa444e105
IEYT <140979013+IEYT@users.noreply.github.com>
2025-04-29 16:23:12 -0600

    Uh TEST

---
+++ game.js
@@ -0,0 +1,6 @@
+async function loadPyodide() {
+  let pyodide = await loadPyodide();
+  pyodide._api._skip_unwind_fatal_error = true; // Add the flag here
+  await pyodide.loadPackage("pygame-ce");
+  return pyodide;
+}
