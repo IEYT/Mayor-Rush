c7bf8fef1fdcfbe68b20631c4f0657fdd761196c
Manveer Singh <140979013+IEYT@users.noreply.github.com>
2025-05-03 11:15:34 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,20 +1,14 @@
 async function main() {
   let pyodide = await loadPyodide();
 
-  pyodide._api._skip_unwind_fatal_error = true; // Add the flag here
+  pyodide._api._skip_unwind_fatal_error = true;
 
   await pyodide.loadPackage("pygame-ce");
-
-  // let sdl2Canvas = document.createElement("canvas");
-  // sdl2Canvas.id = "canvas";
-  // document.getElementsByTagName('body')[0].appendChild(sdl2Canvas);
-  // pyodide.canvas.setCanvas2D(sdl2Canvas);
 
   let sdl2Canvas = document.getElementById("canvas");
   pyodide.canvas.setCanvas2D(sdl2Canvas);
 
-  // pyodide.runPython(`
-  pyodide.runPythonAsync(`
+   pyodide.runPythonAsync(`
   import asyncio
   import pygame
   
@@ -36,7 +30,7 @@ async function main() {
           pygame.display.flip()
   
           clock.tick(FPS)
-          await asyncio.sleep(0)  # should be inside loop
+          await asyncio.sleep(0) 
   
       pygame.quit()
   
