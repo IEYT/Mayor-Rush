5a4dbebc8c1013222c229d00f059af41ee95a61e
IEYT <140979013+IEYT@users.noreply.github.com>
2025-05-02 20:04:33 -0600

    Imported Pygame-CE
    Thanks to Furas, a stackoverflow user, I got this working.

--- game.js
+++ game.js
@@ -1,39 +1,47 @@
 async function main() {
   let pyodide = await loadPyodide();
+
   pyodide._api._skip_unwind_fatal_error = true; // Add the flag here
+
   await pyodide.loadPackage("pygame-ce");
-  let sdl2Canvas = document.createElement("canvas");
-  sdl2Canvas.id = "canvas";
+
+  // let sdl2Canvas = document.createElement("canvas");
+  // sdl2Canvas.id = "canvas";
+  // document.getElementsByTagName('body')[0].appendChild(sdl2Canvas);
+  // pyodide.canvas.setCanvas2D(sdl2Canvas);
+
+  let sdl2Canvas = document.getElementById("canvas");
   pyodide.canvas.setCanvas2D(sdl2Canvas);
-  pyodide.runPython(`
-    import asyncio  
-    import pygame  
 
-    async def run_game():  
-        # pygame setup
-        pygame.init()
-        screen = pygame.display.set_mode((1280, 720))
-        clock = pygame.time.Clock()
-        running = True
-
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-            screen.fill("purple")
-
-
-            pygame.display.flip()
-
-
-        pygame.quit()
-        await asyncio.sleep(1 / fps)
-
-
-
+  // pyodide.runPython(`
+  pyodide.runPythonAsync(`
+  import asyncio
+  import pygame
+  
+  FPS = 60
+  
+  async def run_game():
+      # pygame setup
+      pygame.init()
+      screen = pygame.display.set_mode((1280, 720))
+  
+      clock = pygame.time.Clock()
+      running = True
+      while running:
+          for event in pygame.event.get():
+              if event.type == pygame.QUIT:
+                  running = False
+  
+          screen.fill("green")
+          pygame.display.flip()
+  
+          clock.tick(FPS)
+          await asyncio.sleep(0)  # should be inside loop
+  
+      pygame.quit()
+  
+  run_game()  
     `);
-  return pyodide;
 }
 main();
 console.log("Pygame loads, YAY!");
