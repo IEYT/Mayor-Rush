d3a2cf9a17b38867b7734d9fb7eee7f39551294c
IEYT <140979013+IEYT@users.noreply.github.com>
2025-05-15 20:59:30 -0600

    s

--- game.js
+++ game.js
@@ -1,41 +1,5 @@
-async function main() {
-  let pyodide = await loadPyodide();
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-  pyodide._api._skip_unwind_fatal_error = true;
-
-  await pyodide.loadPackage("pygame-ce");
-
-  let sdl2Canvas = document.getElementById("canvas");
-  pyodide.canvas.setCanvas2D(sdl2Canvas);
-
-   pyodide.runPythonAsync(`
-  import asyncio
-  import pygame
-  
-  FPS = 60
-  
-  async def run_game():
-      # pygame setup
-      pygame.init()
-      screen = pygame.display.set_mode((1280, 720))
-  
-      clock = pygame.time.Clock()
-      running = True
-      while running:
-          for event in pygame.event.get():
-              if event.type == pygame.QUIT:
-                  running = False
-  
-          screen.fill("green")
-          pygame.display.flip()
-  
-          clock.tick(FPS)
-          await asyncio.sleep(0) 
-  
-      pygame.quit()
-  
-  run_game()  
-    `);
-}
-main();
-console.log("Pygame loads, YAY!");
+ctx.fillStyle = "#B2FFBD";
+ctx.fillRect(10, 10, 758, 350);
